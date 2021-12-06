880d431f4abdf12afdb53bb00960411d4e6be60e
Henrique Barros <henriquesbarroshs@gmail.com>
2021-12-06 07:36:37 -0300

    feat: initial navigation flow created

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { 
   Box, 
   Flex, 
@@ -14,7 +15,7 @@ import {
   Text,
 	useBreakpointValue 
 } from "@chakra-ui/react";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { RiAddLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
@@ -35,15 +36,18 @@ export default function UserList() {
 				<Box flex="1" borderRadius={8} bg="gray.800" p="8">
 					<Flex mb="8" justify="space-between" align="center">
 						<Heading size="lg" fontWeight="normal">Usuários</Heading>
-						<Button 
-							as="a" 
-							size="sm" 
-							fontSize="sm" 
-							colorScheme="pink"
-							leftIcon={<Icon as={RiAddLine} fontSize="20"/>}
-						>
-							Criar novo usuário
-						</Button>
+
+						<Link href="users/create" passHref>
+							<Button 
+								as="a" 
+								size="sm" 
+								fontSize="sm" 
+								colorScheme="pink"
+								leftIcon={<Icon as={RiAddLine} fontSize="20"/>}
+							>
+								Criar novo usuário
+							</Button>
+						</Link>
 					</Flex>
 
 					<Table colorScheme="whiteAlpha">

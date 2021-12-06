3e030e532df846e5ebe4c921fb8fc5b643f09907
Henrique Barros <henriquesbarroshs@gmail.com>
2021-12-06 07:21:33 -0300

    feat: responsive listing

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -11,74 +11,71 @@ import {
   Checkbox, 
   Tbody, 
   Td, 
-  Text 
+  Text,
+	useBreakpointValue 
 } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
-import { Sidebar } from "../../components/Sidebar";
+import { SideBar } from "../../components/SideBar";
 
 export default function UserList() {
-    return (
-        <Box>
-          <Header />
-          <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
-            <Sidebar />
-              <Box flex="1" borderRadius={8} bg="gray.800" p="8">
-                <Flex mb="8" justify="space-between" align="center">
-                  <Heading size="lg" fontWeight="normal">Usuários</Heading>
-                  <Button 
-                    as="a" 
-                    size="sm" 
-                    fontSize="sm" 
-                    colorScheme="pink"
-                    leftIcon={<Icon as={RiAddLine} fontSize="20"/>}
-                  >
-                    Criar novo usuário
-                  </Button>
-                </Flex>
-                <Table colorScheme="whiteAlpha">
-                  <Thead>
-                    <Tr>
-                      <Th px="6" color="gray.300" width="8">
-                        <Checkbox colorScheme="pink" />
-                      </Th>
-                      <Th>Usuário</Th>
-                      <Th>Data de cadastro</Th>
-                      <Th wdith="8"></Th>
-                    </Tr>
-                  </Thead>
-                  <Tbody>
-                    <Tr>
-                      <Td px="6">
-                        <Checkbox colorScheme="pink" />
-                      </Td>
-                      <Td>
-                        <Box>
-                          <Text fontWeight="bold">Henrique Barros</Text>
-                          <Text fontSize="sm" color="gray.300">henriquesbarroshs@gmail.com</Text>
-                        </Box>
-                      </Td>
-                      <Td>19 de março de 2021</Td>
-                      <Td alignSelf="end">
-                        <Button 
-                          as="a" 
-                          size="sm" 
-                          fontSize="sm" 
-                          colorScheme="purple"
-                          leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                        >
-                          Editar
-                        </Button>
-                      </Td>
-                    </Tr>
-                  </Tbody>
-                </Table>
+	const isWideVersion = useBreakpointValue({
+		base: false,
+		lg: true
+	});
 
-                <Pagination />
-              </Box>
-            </Flex>
-        </Box>
-    );
+	return (
+		<Box>
+			<Header />
+			<Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+				<SideBar />
+
+				<Box flex="1" borderRadius={8} bg="gray.800" p="8">
+					<Flex mb="8" justify="space-between" align="center">
+						<Heading size="lg" fontWeight="normal">Usuários</Heading>
+						<Button 
+							as="a" 
+							size="sm" 
+							fontSize="sm" 
+							colorScheme="pink"
+							leftIcon={<Icon as={RiAddLine} fontSize="20"/>}
+						>
+							Criar novo usuário
+						</Button>
+					</Flex>
+
+					<Table colorScheme="whiteAlpha">
+						<Thead>
+							<Tr>
+								<Th px={["4", "4", "6"]} color="gray.300" width="8">
+									<Checkbox colorScheme="pink" />
+								</Th>
+								<Th>Usuário</Th>
+								{ isWideVersion && <Th>Data de cadastro</Th> }
+							</Tr>
+						</Thead>
+
+						<Tbody>
+							<Tr>
+								<Td px={["4", "4", "6"]}>
+									<Checkbox colorScheme="pink" />
+								</Td>
+								<Td>
+									<Box>
+										<Text fontWeight="bold">Henrique Barros</Text>
+										<Text fontSize="sm" color="gray.300">henriquesbarroshs@gmail.com</Text>
+									</Box>
+								</Td>
+								{ isWideVersion && <Td>19 de março de 2021</Td> }
+							</Tr>
+						</Tbody>
+					</Table>
+
+					<Pagination />
+				</Box>
+			</Flex>
+		</Box>
+	);
 }

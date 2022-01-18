f9657d5180cfd4747b07dbc64a8804088ecf6599
Henrique Barros <henriquesbarroshs@gmail.com>
2022-01-18 09:03:57 -0300

    feat: add ssr in react query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { GetServerSideProps } from 'next';
 import NextLink from 'next/link';
 import { 
   Box, 
@@ -21,16 +22,18 @@ import {
 import { RiAddLine } from "react-icons/ri";
 
 import { api } from '../../services/api';
-import { useUsers } from '../../services/hooks/useUsers';
+import { getUsers, useUsers } from '../../services/hooks/useUsers';
 import { queryClient } from '../../services/queryClient';
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { SideBar } from "../../components/SideBar";
 
-export default function UserList() {
+export default function UserList({ users }) {
 	const [page, setPage] = useState(1);
-	const { data, isLoading, isFetching, error } = useUsers(page);
+	const { data, isLoading, isFetching, error } = useUsers(page, {
+		initialData: users
+	});
 
 	const isWideVersion = useBreakpointValue({
 		base: false,
@@ -127,4 +130,14 @@ export default function UserList() {
 			</Flex>
 		</Box>
 	);
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+	const { users, totalCount } = await getUsers(1);
+
+	return {
+		props: {
+			users,
+		}
+	}
 }
